8ba5a04ca0f8b76c9aca8ddbe552f5cf73c4e8b9
harini-spec <ponshriharini@gmail.com>
2024-08-04 12:14:21 +0530

    Updated redirect uri

--- src/Components/OAuth/GoogleFitComponent.js
+++ src/Components/OAuth/GoogleFitComponent.js
@@ -10,6 +10,7 @@ function GoogleFitComponent({ handleLoginSuccess }) {
 
       // Exchange authorization code for access token
       
+      console.log('Authorization code:', response.code);
       const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
         method: 'POST',
         headers: {
@@ -19,11 +20,12 @@ function GoogleFitComponent({ handleLoginSuccess }) {
           code: response.code,
           client_id: Secret.clientId,
           client_secret: Secret.clientSecret,
-          redirect_uri: 'http://localhost:3000',
+          redirect_uri: 'https://salmon-mushroom-0e8dc301e.5.azurestaticapps.net',
           grant_type: 'authorization_code',
         }),
       });
 
+      
       const tokenData = await tokenResponse.json();
       if (tokenData.access_token) {
         handleLoginSuccess(tokenData.access_token);
@@ -36,7 +38,7 @@ function GoogleFitComponent({ handleLoginSuccess }) {
     },
     flow: 'auth-code', // Explicitly using authorization code flow
     scope: 'https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.heart_rate.read https://www.googleapis.com/auth/fitness.nutrition.read https://www.googleapis.com/auth/fitness.body.read https://www.googleapis.com/auth/fitness.blood_pressure.read https://www.googleapis.com/auth/fitness.sleep.read',
-    redirect_uri: 'http://localhost:3000',
+    redirect_uri: 'https://salmon-mushroom-0e8dc301e.5.azurestaticapps.net',
   });
 
   return (
